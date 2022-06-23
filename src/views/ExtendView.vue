ab15b8b12f57f55355843d55acd8426fccfc7bba
chiruno <84932886+thanhmr2k@users.noreply.github.com>
2022-06-24 01:10:01 +0700

    Update ExtendView.vue

--- src/views/ExtendView.vue
+++ src/views/ExtendView.vue
@@ -1,21 +1,6 @@
 <template>
-    <div class="intro">
-        <h2>Đặng Đình Thành - B19DCCN648</h2>
-        <h1>Mô tả</h1>
-        <p>Đây là web em xây dựng để cập nhật thông tin thời tiết và dự báo thời tiết trong 7 ngày với tên thành phố nhập vào, có phần map với graph</p>
-        <h1>Tính năng</h1>
-        <p>Khi nhập tên thành phố/ địa điểm vào thanh tìm kiếm và bấm enter, data tên thành phố dạng text nhập vào được đưa vào hàm js fetchWeather để xử lý. 
-            API em sử dụng là https://weatherdbi.herokuapp.com/data/weather/{địa điểm}.
-            API này cung cấp data lẫn hình ảnh để biểu diễn trên thanh chart và weather-wrap</p>
-        <h1>Công nghệ</h1>
-        <p>Project làm từ Vue.js, cụ thể ứng dụng các công nghệ như sau:</p>
-        <ul class="a">
-            <li>vue-chart.js</li>
-            <li>vue-animation</li>
-            <li>vue-CLI</li>
-            <li>Vue single file component</li>
-            <li>Vue route</li>
-        </ul>
+    <div >
+        <h1>Hi</h1>
     </div>
 </template>
 <script>
